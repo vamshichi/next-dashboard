0bef3554fc626a200058cd518bc760ac7d3ec0f9
vamshichi <chvamshi03@gmail.com>
2024-07-30 19:22:07 +0530

    uploded complete project

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,10 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
+            {/* <a href=" " className="text-blue-500">
+              login with email = user@nextmail.com and password = 123456 to explore the project.
+            </a> */}
+            login with email = user@nextmail.com and password = 123456 to explore the project.
             , brought to you by Vercel.
           </p>
           <Link
